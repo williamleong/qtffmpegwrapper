8d21f5c4605b652c5ed97c8dc2ee3013b04b5252
William Leong <tsllwl@nus.edu.sg>
2017-04-24 12:54:05 +0800

    Cleanup files that will not be updated

--- packagesrc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-del qtffmpegwrapper_src.zip
-
-zip -r qtffmpegwrapper_src.zip doc -x *.bak
-zip -r qtffmpegwrapper_src.zip example -x *.bak -x example/build-*
-zip -r qtffmpegwrapper_src.zip QTFFmpegWrapper -x *.bat
-zip qtffmpegwrapper_src.zip * -x *.bat -x .svn/ -x *.bak -x *.zip
-
